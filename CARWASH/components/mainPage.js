08b09e7a7c6f960389beef3cd6556c76ed2f32f4
FCHCC <fccachon@hotmail.com>
2018-09-20 13:01:30 -0500

    fixing menu

--- CARWASH/components/mainPage.js
+++ CARWASH/components/mainPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View,StyleSheet,TouchableHighlight,ScrollView,Image} from 'react-native';
+import {Text, View,StyleSheet,TouchableHighlight,ScrollView,Image,FlatList} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import ServiceMenu from '../components/serviceMenu.js';
 
@@ -13,20 +13,42 @@ import ServiceMenu from '../components/serviceMenu.js';
       <View style={{flex:1, backgroundColor:'white'}} >
 
         <View style={{height:200}} >
-        <ScrollView horizontal pagingEnabled >
+        <FlatList horizontal={true}
+          data={promos}
+          renderItem={({item})=>
           <View style={{width:420,height:200, backgroundColor:'white'}}>
 
-          <Image style={{flex:1,height: undefined,width:undefined}} resizeMode="contain" source={require("../images/promocion1.png")} />
+          <Image style={{flex:1,height: undefined,width:undefined}} resizeMode="contain" source={item.urlImage} />
 
           </View>
-        </ScrollView>
+
+        }
+        keyExtractor={(item, index) => index.toString()}
+      />
         </View>
 
         <View>
           <Text style={styles.textTitle}>HAZ CLIC EN EL SERVICIO DESEADO</Text>
       </View>
 
-        <ScrollView>
+
+      <FlatList
+            data={servicios}
+            renderItem={({item,index})=>
+            <View>
+              <TouchableHighlight index={index} style={styles.button} underlayColor="white">
+                <View style={{flexDirection:'row'}}>
+                    <Image style={styles.imageMenu} source={item.urlImage}/>
+                  <Text style={styles.buttonText}>{item.service}</Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+          }
+          keyExtractor={(item, index) => index.toString()}
+        />
+
+
+      {/*  <ScrollView>
 
             <ServiceMenu urlImage={require("../images/washingcar.png")} serviceName='LAVADO'/>
 
@@ -54,13 +76,79 @@ import ServiceMenu from '../components/serviceMenu.js';
 
             <ServiceMenu serviceName='DESMANCHADO POR ZONA' urlImage={require("../images/stained.png")}/>
         </ScrollView>
-
+*/}
       </View>
 
     );
   }
 
 }
+
+var promos=[
+  {
+    urlImage:require("../images/promocion1.png")
+  },
+  {
+    urlImage:require("../images/promocion2.png")
+  },
+  {
+    urlImage:require("../images/LOGOCARWASH.jpg")
+  }
+]
+var servicios=[
+  {
+    service:'LAVADO',
+    urlImage: require("../images/washingcar.png")
+  },
+  {
+    service:'ASPIRADO',
+    urlImage: require("../images/vacuum-cleaner.png")
+  },
+  {
+    service:'BAÑO DE CERA',
+    urlImage:require("../images/carclean.png"),
+  },
+  {
+    service:'LAVADO DE MOTOR',
+    urlImage:require("../images/engine.png"),
+  },
+  {
+    service:'PULIDO',
+    urlImage:require("../images/car1.png"),
+  },
+  {
+    service:'HIDRATACION DE INTERIORES',
+    urlImage:require("../images/seat.png"),
+  },
+  {
+    service:'LAVADO SALPICADERAS',
+    urlImage:require("../images/carfront.png"),
+  },
+  {
+    service:'DESCONTAMINADO',
+    urlImage:require("../images/car2.png"),
+  },
+  {
+    service:'ENCERADO',
+    urlImage:require("../images/wax.png"),
+  },
+  {
+    service:'LAVADO VESTIDURAS Y CIELO',
+    urlImage: require("../images/wax.png"),
+  },
+  {
+    service:'PULIR FAROS',
+    urlImage:require("../images/high-beam.png"),
+  },
+  {
+    service:'PULIR RINES',
+    urlImage:require("../images/cartire.png"),
+  },
+  {
+    service:'DESMANCHADO POR ZONA',
+    urlImage:require("../images/stained.png"),
+  }
+  ];
 
 const styles = StyleSheet.create({
 
@@ -70,6 +158,32 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     textAlign:'center',
     color:'#343a8b',
+  },
+
+  button:{
+    backgroundColor:'#343a8b',
+    flexDirection:'row',
+    height:80,
+    borderWidth:2,
+    borderColor:'#2c67b2',
+    justifyContent:'space-between',
+    padding:10,
+    marginTop:10,
+
+  },
+  buttonText:{
+
+    color:'white',
+    fontSize:20,
+    fontWeight:'bold',
+    paddingTop:10,
+    paddingLeft:30,
+
+  },
+  imageMenu:{
+    width:60,
+    height:60,
+    paddingLeft:30,
   }
 })
 
